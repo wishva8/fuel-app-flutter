6518e9cad1c63cfa6a06201c7e42d866c3cbd84a
Wishva <vibushitha99@gmail.com>
2022-10-26 23:26:32 +0530

    stat page

--- fuel_app/lib/pages/station_stat.dart
+++ fuel_app/lib/pages/station_stat.dart
@@ -10,7 +10,7 @@ import 'package:fuel_app/model/station_stat.dart';
 
 class StationDetails extends StatelessWidget {
   final StationStat station;
-  const StationDetails({ Key? key,required this.station }) : super(key: key);
+  const StationDetails({Key? key, required this.station}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +21,159 @@ class StationDetails extends StatelessWidget {
       body: Container(
         child: ListView(
           children: [
-            Text(""+station.location)
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Container(
+                height: 150.0,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    image: DecorationImage(
+                        image: NetworkImage(
+                          "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/1539637/gas-station-clipart-md.png",
+                        ),
+                        fit: BoxFit.fitHeight)),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  children: [
+                    Text(
+                      "Location :  " + station.location,
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                )),
+            Container(
+                margin: EdgeInsets.only(left: 20, right: 0, top: 30),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Petrol Status",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Availability : " + (station.petrol == 1 ? " ✔ " : " ❌ "),
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Waiting Time : " +
+                          (station.disal == 1
+                              ? station.petrolWaitingTime + " min"
+                              : "No queue"),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        OutlineButton(
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("Exit Queue",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  letterSpacing: 2.2,
+                                  color: Colors.black)),
+                        ),
+                        RaisedButton(
+                          onPressed: () async {},
+                          color: Colors.green,
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Text(
+                            "Enter Queue",
+                            style: TextStyle(
+                                fontSize: 14,
+                                letterSpacing: 2.2,
+                                color: Colors.white),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )),
+            Container(
+                margin: EdgeInsets.only(left: 20, right: 00, top: 30),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Disel Status",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Availability : " + (station.disal == 1 ? " ✔ " : " ❌ "),
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Waiting Time : " +
+                          (station.disal == 1
+                              ? station.disalWaitingTime + " min"
+                              : "No queue"),
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        OutlineButton(
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text("Exit Queue",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  letterSpacing: 2.2,
+                                  color: Colors.black)),
+                        ),
+                        RaisedButton(
+                          onPressed: () async {},
+                          color: Colors.green,
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          elevation: 2,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Text(
+                            "Enter Queue",
+                            style: TextStyle(
+                                fontSize: 14,
+                                letterSpacing: 2.2,
+                                color: Colors.white),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )),
           ],
         ),
       ),
